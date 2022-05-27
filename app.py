29a25d528c0e539247c7ceb22fd5a8caf83043bc
Matt Stiller <matthewstiller484@yahoo.com>
2022-05-27 12:25:11 -0400

    Render the same template if / or /home.

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from graphviz import render
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World"
+    return render_template("base.html")
+
+@app.route("/home")
+def hello_world():
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
